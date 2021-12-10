d0aaa171666e86da2481ca0c2cfcd95f76ef22e1
webioann <ioannn7777@gmail.com>
2021-12-10 16:47:50 +0200

    create 'data' folder and transferred the files

---
+++ src/data/multiLang.js
@@ -0,0 +1,34 @@
+export const lang = {
+    eng: {
+        month: {
+            jan: 'Jan',
+            feb: 'Feb',
+            mar: 'Mar',
+            apr: 'Apr', 
+            may: 'May',
+            jun: 'Jun',
+            jul: 'Jul',
+            aug: 'Aug',
+            sep: 'Sep',
+            oct: 'Oct',
+            nov: 'Nov',
+            dec: 'Dec'
+        }
+    },
+    rus: {
+        month: {
+            jan: 'Янв',
+            feb: 'Фев',
+            mar: 'Мар',
+            apr: 'Апр', 
+            may: 'Май',
+            jun: 'Июн',
+            jul: 'Июл',
+            aug: 'Авг',
+            sep: 'Сен',
+            oct: 'Окт',
+            nov: 'Ноя',
+            dec: 'Дек'
+        }
+    }
+}
---
+++ src/data/nations.js
@@ -0,0 +1,22 @@
+const nations = [
+    {code:'ALL',name:"Anybody",     color:'blue'},
+    {code:'BR', name:"Brazilian",   color:'green'},
+    {code:'AU', name:"Australian",  color:'red'},
+    {code:'CA', name:"Canadian",    color:'yellow'},
+    {code:'CH', name:"Chinese",     color:'grey'},
+    {code:'DE', name:"German",      color:'pink'},
+    {code:'DK', name:"Danish",      color:'green'},
+    {code:'ES', name:"Spanish",     color:'green'},
+    {code:'FI', name:"Finnish",     color:'green'},
+    {code:'FR', name:"Franch",      color:'green'},
+    {code:'GB', name:"British",     color:'green'},
+    {code:'IE', name:"Irish",       color:'green'},
+    {code:'IR', name:"Iranian",     color:'green'}, 
+    {code:'NO', name:"Norwegian",   color:'green'},
+    {code:'NL', name:"Dutch",       color:'green'},
+    {code:'NZ', name:"New Zealand", color:'green'},
+    {code:'TR', name:"Turkish",     color:'green'},
+    {code:'US', name:"American",    color:'green'},
+]
+    
+export default nations;
