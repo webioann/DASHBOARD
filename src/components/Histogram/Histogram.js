9d5a7fed8079ea67ffe941f12f857d74b933fb3e
webioann <ioannn7777@gmail.com>
2021-12-25 23:48:17 +0200

    connected multilanguage in Histogram.js

--- src/components/Histogram/Histogram.js
+++ src/components/Histogram/Histogram.js
@@ -1,16 +1,23 @@
-import React,{useState,useEffect} from 'react';
-import './histogram.css'
+import React,{useState,useEffect} from 'react'
+import { useSelector } from "react-redux"
 import  nations  from '../../data/nations'
+import './histogram.css'
 
 function Histogram() {
 
+    const lang = useSelector((state) => state.dialect.lang)
     nations.map(item => { Object.assign( item , { size: 200 } )})
 
     return (
         <ul className="chart">
+            <div className="title-for">
+                { lang === 'eng' ? nations[0].nameEng : nations[0].nameRus }
+            </div>
             { nations.map( unit  => { if (unit.code !== 'ALL') {
                 return <li className="chart-item" key={unit.code} >
-                    <span className="chart-title">{unit.nameEng}</span>
+                    <span className="chart-title">
+                        { lang === 'eng' ? unit.nameEng : unit.nameRus }
+                    </span>
                     <div className="histogram" 
                         style={{ backgroundColor: unit.color, width: `${unit.size}px`}}>
                     </div> 
