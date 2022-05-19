0357a1cd2febbc5a9d6e6540fbc23ebf753fa8bc
webioann <ioannn7777@gmail.com>
2022-05-19 09:36:07 +0300

    works with favicon for site

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require("copy-webpack-plugin")
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
     mode: "development", 
     entry: "./src/index.js",
     output: {
         filename: "[name].[hash:4].js", 
-        path: path.resolve( __dirname,"dist" ),
+        path: path.resolve( __dirname,"build" ),
     },
     devServer: {
         port: 8001,
@@ -22,9 +23,10 @@ module.exports = {
         }),
         new CopyPlugin({
             patterns: [
-            { from: "./src/assets", to: "./static" },
+                { from: "./src/assets", to: "./static" },
             ],
         }),
+        new FaviconsWebpackPlugin('./src/assets/favicon.png'),
     ],
     module: {
         rules: [
