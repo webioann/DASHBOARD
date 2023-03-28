1f3d646de41c9d1244a96e340520ff365fc2a5dc
Webioann <ioannn7777@gmail.com>
2023-03-28 15:57:11 +0300

    redoo

--- README.md
+++ README.md
@@ -1,18 +1,3 @@
-## Getting Started
-        to print in console:
-        $ yarn add -D webpack webpack-cli
-        $ yarn add -D mini-css-extract-plugin css-loader sass-loader sass file-loader
-        $ yarn add -D html-webpack-plugin clean-webpack-plugin 
-        $ yarn add -D copy-webpack-plugin 
-        $ yarn add -D terser-webpack-plugin 
-        $ yarn add -D webpack-dev-server
-        $ yarn add -D babel-loader @babel/core
-        $ yarn add -D @babel/preset-env 
-        $ yarn add -D @babel/preset-react
-        $ yarn add react react-dom
+# Users dashboard on > React
 
-2. create directory <span style="color:yellow">npm</span> **src /../../** <br>
-
-
-
-
+### start app 🚀 `npm start`
--- src/hooks/useGenderHistogram.js
+++ src/hooks/useGenderHistogram.js
@@ -1,25 +1,24 @@
-import { useState,useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 const useGenderHistogram = () => {
-
-    const data = useSelector((state) => state.redux.data)
-    const dataVolum = data.length
-    const [totalMen,setTotalMen]  = useState('')
-    const [totalWomen,setTotalWomen] = useState('')
+    const data = useSelector((state) => state.redux.data);
+    const dataVolum = data.length;
+    const [totalMen, setTotalMen] = useState('');
+    const [totalWomen, setTotalWomen] = useState('');
 
     useEffect(() => {
-        const M = data.filter(item => item.gender === 'male')
-        const MEN = M.length
-        const W = data.filter(item => item.gender === 'female')
-        const WOMEN = W.length
-        let onePercentage = dataVolum / 100
-        let RAW = Math.round(MEN / onePercentage)
-        setTotalMen( RAW )
-        setTotalWomen( 100 - RAW )
-    },[data])
+        const M = data.filter((item) => item.gender === 'male');
+        const MEN = M.length;
+        const W = data.filter((item) => item.gender === 'female');
+        const WOMEN = W.length;
+        let onePercentage = dataVolum / 100;
+        let RAW = Math.round(MEN / onePercentage);
+        setTotalMen(RAW);
+        setTotalWomen(100 - RAW);
+    }, [data]);
 
-    return { totalMen, totalWomen }
-}
+    return { totalMen, totalWomen };
+};
 
 export default useGenderHistogram;
